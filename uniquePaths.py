a8bb0ad885bb26d2dd2c1335c9160af17c4c7a66
Yan <Yan.Shlain@gmail.com>
2024-06-17 18:06:01 +0300

    Update uniquePaths.py

--- uniquePaths.py
+++ uniquePaths.py
@@ -1,1 +1,17 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # I will try to resolve this question using recursion
+        # and keeping results in the set
 
+        if not m or not n:
+            return 0
+
+        if m == 1 and n == 1:
+            return 1
+
+        # The calculation will be in the opposite direction,
+        # because the total paths are the same.
+        total = self.uniquePaths(m-1, n)
+        total += self.uniquePaths(m, n-1)
+
+        return total
